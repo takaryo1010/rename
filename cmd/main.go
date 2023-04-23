ae9666b9b479f23341d3f47bf33aa3106580684d
takaryo1010 <t.ryota1010@gmail.com>
2023-04-23 15:49:11 +0900

    add:part.go

--- cmd/main.go
+++ cmd/main.go
@@ -12,7 +12,7 @@ func main() {
 
 	var (
 		a = flag.String("a", "", "Renames all files in the specified directory to the argument + number.\nUsage:'-a NewName")
-		p = flag.String("p", "", "Changes the specified part of a file name in the specified directory to the argument.\nUsage:'-p NewName")
+		p = flag.String("p", "", "Changes the specified part of a file name in the specified directory to the argument.\nUsage:'-p FromName,ToName")
 	)
 	flag.Usage = func() {
         fmt.Fprintf(os.Stderr, "Usage: %s -p or -a [ARGUMENTS] [PATH] \n", os.Args[0])
@@ -37,6 +37,7 @@ func main() {
 
 	}else if *p !=""{
 		// func
+		rename.RenamePart(flag.Arg(0),*p)
 		os.Exit(0)
 	}
 
--- internal/rename/part.go
+++ internal/rename/part.go
@@ -1,2 +1,33 @@
 package rename
 
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func RenamePart(root string, NewName string) {
+
+	Names:=strings.Split(NewName, ",")
+	var (
+		FromName string = Names[0]
+		ToName string = Names[1]
+	)
+	if len(Names)!=2{
+		return 
+	}
+	
+    filepath.Walk(root, func(path string, info fs.FileInfo, err error) error {
+        if info.IsDir() {
+            return nil
+        }
+        newPath := filepath.Join(root, info.Name())
+		newFilePath := strings.Replace(path, FromName,ToName, -1)
+        err = os.Rename(newPath, newFilePath)
+        if err != nil {
+            return nil
+        }
+        return nil
+    })
+}
